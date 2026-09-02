56d79bbd4b617f5187565728505110343955baeb
Laura Rice <gabbykitty2000@gmail.com>
2026-09-02 19:07:25 -0400

    Set up Open API JavaScript DOM elements

---
+++ js/openapi.js
@@ -0,0 +1,10 @@
+// Test JavaScript connection
+console.log("Open API JavaScript is working.");
+
+// Select containers for cat results and cat information
+const catResults = document.getElementById("cat-results");
+const catInfo = document.getElementById("cat-info");
+
+// Test HTML element selection
+console.log(catResults);
+console.log(catInfo);
