6c56a0200c3538520bf0c9e8b60ff31cbfee00f1
Laura Rice <gabbykitty2000@gmail.com>
2026-09-05 17:37:25 -0400

    Connect new cats to detail display

--- js/openapi.js
+++ js/openapi.js
@@ -11,6 +11,19 @@ console.log(catResults);
 console.log(catInfo);
 console.log(exploreCatsLink);
 
+// ----- ENDPOINT 1: CAT IMAGE SEARCH -----
+
+// Store the API URL for the cat image search request
+const searchURL = "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&limit=6";
+
+// Set up the API request options
+const requestOptions = {
+    method: "GET",
+    headers: {
+        "x-api-key": "live_KcKz112YhgHDjzFW8h1S9BCLUHjIC5OyFftHxvAQyAlZXWo2ykAScieo0gpoPP9f"
+    }
+};
+
 // Test Explore Cats navigation
 exploreCatsLink.addEventListener("click", () => {
     console.log("Explore Cats link clicked");
@@ -35,9 +48,103 @@ exploreCatsLink.addEventListener("click", () => {
                 // Set the image source to the new cat's URL
                 newCatImage.src = cat.url;
 
-                // Identify the new selected cat
+                // Identify the selected new cat
                 newCatImage.addEventListener("click", () => {
                     console.log(cat.id);
+
+                    // Create the detail URL for the selected new cat
+                    const detailURL = `https://api.thecatapi.com/v1/images/${cat.id}`;
+
+                    console.log(detailURL);
+
+                    // Request details for the selected new cat
+                    fetch(detailURL, requestOptions)
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            console.log(data);
+
+                            // Create an image element for the selected new cat
+                            const detailImage = document.createElement("img");
+
+                            // Set the image source to the selected new cat's URL
+                            detailImage.src = data.url;
+                            console.log(detailImage);
+
+                            // Add the selected new cat's image to the details section
+                            catInfo.appendChild(detailImage);
+
+                            // Get the selected new cat's breed name
+                            console.log("Breed:", data.breeds[0].name);
+
+                            const breedName = document.createElement("h3");
+                            breedName.textContent = "Breed: " + data.breeds[0].name;
+                            console.log(breedName);
+
+                            // Add the breed name to the details section
+                            catInfo.appendChild(breedName);
+
+                            // Get the selected new cat's breed group
+                            console.log("Breed Group: ", data.breeds[0].breed_group);
+
+                            const breedGroup = document.createElement("p");
+                            breedGroup.textContent = "Breed Group: " + data.breeds[0].breed_group;
+                            console.log(breedGroup);
+
+                            // Add the breed group to the details section
+                            catInfo.appendChild(breedGroup);
+
+                            // Get the selected new cat's temperament
+                            console.log("Temperament: ", data.breeds[0].temperament);
+
+                            const temperamentText = document.createElement("p");
+                            temperamentText.textContent = "Temperament: " + data.breeds[0].temperament;
+                            console.log(temperamentText);
+
+                            // Add the temperament to the details section
+                            catInfo.appendChild(temperamentText);
+
+                            // Get the selected new cat's origin
+                            console.log("Origin: ", data.breeds[0].origin);
+
+                            const origin = document.createElement("p");
+                            origin.textContent = "Origin: " + data.breeds[0].origin;
+                            console.log(origin);
+
+                            // Add the origin to the details section
+                            catInfo.appendChild(origin);
+
+                            // Get the selected new cat's life span
+                            console.log("Life Span: ", data.breeds[0].life_span);
+
+                            const lifeSpan = document.createElement("p");
+                            lifeSpan.textContent = "Life Span: " + data.breeds[0].life_span;
+                            console.log(lifeSpan);
+
+                            // Add the life span to the details section
+                            catInfo.appendChild(lifeSpan);
+
+                            // Get the selected new cat's description
+                            console.log("Description: ", data.breeds[0].description);
+
+                            const description = document.createElement("p");
+                            description.textContent = "Description: " + data.breeds[0].description;
+                            console.log(description);
+
+                            // Add the description to the details section
+                            catInfo.appendChild(description);
+
+                            // Get the selected new cat's history
+                            console.log("History: ", data.breeds[0].history);
+
+                            const history = document.createElement("p");
+                            history.textContent = "History: " + data.breeds[0].history;
+                            console.log(history);
+
+                            // Add the history to the details section
+                            catInfo.appendChild(history);
+                        });
                 });
 
                 console.log(newCatImage);
@@ -47,19 +154,6 @@ exploreCatsLink.addEventListener("click", () => {
             });
         });
 });
-
-// ----- ENDPOINT 1: CAT IMAGE SEARCH -----
-
-// Store the API URL for the cat image search request
-const searchURL = "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&limit=6";
-
-// Set up the API request options
-const requestOptions = {
-    method: "GET",
-    headers: {
-        "x-api-key": "live_KcKz112YhgHDjzFW8h1S9BCLUHjIC5OyFftHxvAQyAlZXWo2ykAScieo0gpoPP9f"
-    }
-};
 
 // Request and process cat data from TheCatAPI
 fetch(searchURL, requestOptions)
